dbd21cdb6ff331ed7ae00096cd4ac56099123845
xtx123456 <107483665+xtx123456@users.noreply.github.com>
2023-08-15 23:13:03 +0800

    Add files via upload

---
+++ vcs/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/alinush/go-mcl"
+	"github.com/hyperproofs/hyperproofs-go/vcs"
+)
+
+func main() {
+	testing.Init()
+	flag.Parse()
+
+	fmt.Println("Hello, World!")
+	mcl.InitFromString("bls12-381")
+
+	dt := time.Now()
+	fmt.Println("Specific date and time is: ", dt.Format(time.UnixDate))
+
+	fmt.Println(vcs.SEP)
+
+	args := os.Args
+
+	if len(args) == 1 {
+		var L uint8
+		L = uint8(5)
+		_ = hyperGenerateKeys(L, false)
+
+		L = uint8(12)
+		_ = hyperGenerateKeys(L, true)
+	} else {
+		if args[1] == "1" {
+			snarks_verifier()
+		} else {
+			Benchmark() // Uncomment this benchmark Commit and OpenAll.
+		}
+	}
+}
+
+func hyperGenerateKeys(L uint8, fake bool) *vcs.VCS {
+
+	N := uint64(1) << L
+	vcs := vcs.VCS{}
+
+	fmt.Println("L:", L, "N:", N)
+	folderPath := fmt.Sprintf("pkvk-%02d", L)
+	if fake {
+		vcs.KeyGenFake(5, L, folderPath, 1<<12)
+	} else {
+		vcs.KeyGen(5, L, folderPath, 1<<12)
+	}
+
+	fmt.Println("KeyGen ... Done")
+	return &vcs
+}
+
+func hyperLoadKeys(L uint8) *vcs.VCS {
+
+	folderPath := fmt.Sprintf("pkvk-%02d", L)
+	vcs := vcs.VCS{}
+
+	vcs.KeyGenLoad(5, L, folderPath, 1<<12)
+
+	fmt.Println("KeyGenLoad ... Done")
+	return &vcs
+}
